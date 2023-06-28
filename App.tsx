813ff7a0588917e7ef5e3dae5b742c6d5f7bdab4
Ashutosh Shrestha <ashutoshshrestha@Ashutoshs-MacBook-Pro.local>
2023-06-28 23:10:18 +0545

    added spash screen

--- App.tsx
+++ App.tsx
@@ -10,10 +10,12 @@ import {NavigationContainer} from '@react-navigation/native';
 import {RootStackScreen} from './src/routes/RootStackScreen';
 import {Provider} from 'react-redux';
 import {store} from './src/model/store';
+import SplashScreen from 'react-native-splash-screen';
 
 class App extends React.Component {
   constructor(props: {} | Readonly<{}>) {
     store.dispatch.httpClient.initializeHttpClient();
+    SplashScreen.hide();
     super(props);
   }
   render() {
